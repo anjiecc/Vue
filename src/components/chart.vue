569328f9c6148d74b6000a322958fca56315a74a
chenanjie <chenanjievip@163.com>
2017-08-07 20:29:04 +0800

    修改文件上传功能
    修改文件上传功能
    
    Signed-off-by: chenanjie <chenanjievip@163.com>

---
+++ src/components/chart.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="chart">
+    
+    </div>
+</template>
+<script>
+import echarts from 'echarts';
+
+export default {
+    data() {
+
+    },
+    mounted() {
+        this.init();
+    },
+    methods: {
+        init() {
+           
+            // 基于准备好的dom，初始化echarts实例
+            var myChart = echarts.init(document.getElementById('chart'));
+            // 绘制图表
+            myChart.setOption({
+                title: { text: 'ECharts 入门示例' },
+                tooltip: {},
+                xAxis: {
+                    data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+                },
+                yAxis: {},
+                series: [{
+                    name: '销量',
+                    type: 'bar',
+                    data: [5, 20, 36, 10, 10, 20]
+                }]
+            });
+        }
+    }
+}
+</script>
+<style>
+#chart{
+    width: 100%;
+    height: 100%;
+}
+</style>
+
---
+++ src/components/uploadJs.vue
@@ -0,0 +1,92 @@
+<template>
+    <div id="uploadJs">
+        <div class="grid-content bg-purple"></div>
+        <el-upload class="upload-demo" action="/file/upload" :on-preview="handlePreview" :on-remove="handleRemove" :file-list="fileList2" list-type="picture" :before-upload="beforeLoad" :on-success="uploadSuccess">
+            <el-button size="small" type="primary">点击上传</el-button>
+            <div slot="tip" class="el-upload__tip">只能上传text/markdown文件，列表会显示最近一天添加的文章</div>
+        </el-upload>
+    </div>
+</template>
+
+<script>
+const typeArray = ['text/markdown'];
+export default {
+    data() {
+        return {
+            fileList2: [],
+            author: sessionStorage.getItem("userName")
+        };
+    },
+    mounted() {
+        this._init();
+    },
+    methods: {
+        _init() { //初始化获取全部文件数据
+            this.$http.get("/file/getFile?fileType=md").then(data => {
+                console.log(data);
+                var currentTime = new Date().getTime() - 86400000;
+
+                if (data.body && data.body.msg) {
+                    _.each(data.body.data.data, (item => {
+                        var _time = new Date(item.date).getTime();
+                        if (currentTime < +item.id.split("'")[1]) {
+                            this.fileList2.push({
+                                name: item.name,
+                                url: item.url
+                            });
+                        }
+                    }));
+                }
+            });
+        },
+        handleRemove(file, fileList) { //删除文件回调
+            // console.log(file, fileList);
+        },
+        handlePreview(file) { //点击已上传的文件链接时回调
+            // console.log(file);
+        },
+        beforeLoad(file) { //上传之前回调
+            if (typeArray.indexOf(file.type) < 0) {
+                this.$notify.error({
+                    title: '错误',
+                    message: '目前只支持text/markdown格式的文件!'
+                });
+                return false;
+            }
+
+        },
+        uploadSuccess(res, file, fileList) { // 上传成功回调
+            if (res.type) {
+                var _url = `/file/addFile?id='${new Date().getTime()}'&name=${file.name}&url=${res.originalUrl}&size=${res.size}&date=${new Date(new Date().getTime()).toLocaleString()}&author=${this.author.split(';')[0]}&type=${res.headers['content-type']}&fileType=md`;
+                this.$http.get(_url).then(data => {
+                    console.log(data);
+                    if (data.body.data) {
+                        this.$notify({
+                            title: '成功',
+                            message: '添加文章成功!',
+                            type: 'success'
+                        });
+                    }
+                });
+            }
+        }
+    }
+}
+</script>
+
+<style>
+#uploadJs input[type="file"] {
+    display: none;
+}
+
+#uploadJs .upload-demo,
+#addPhoto .el-upload__tip {
+    text-align: left;
+}
+
+#uploadJs .el-upload.el-upload--picture {
+    margin: 5px 0 0 5px;
+}
+</style>
+
+
